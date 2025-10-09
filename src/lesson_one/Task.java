080c324f6bf5eb308588ff198ba5d6cb7d0da5ec
alsu-ismayilzada <ismayilzadealsu@gmail.com>
2025-10-09 22:20:37 +0400

    Initial commit

---
+++ src/lesson_one/Task.java
@@ -0,0 +1,23 @@
+package lesson_one;
+
+public class Task {
+
+    public static void main(String[] args) {
+        //autoboxing
+
+        int primitiveInt = 10;
+        Integer wrapperInt;
+        wrapperInt = primitiveInt;
+
+        //unboxing
+
+        Integer anotherWrapperInt = 25;
+        int anotherPrimitiveInt = anotherWrapperInt;
+
+        System.out.println("primitiveInt: " + primitiveInt);
+        System.out.println("wrapperInt: " + wrapperInt);
+        System.out.println("anotherWrapperInt: " + anotherWrapperInt);
+        System.out.println("anotherPrimitiveInt: " + anotherPrimitiveInt);
+
+    }
+}
